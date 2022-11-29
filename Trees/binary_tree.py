4bba983fcc76f67605a1f46f6f3c8862f2041812
woahlucas <lucasmelo81@outlook.com>
2022-11-28 22:51:34 -0300

    att arvore binaria

--- Trees/binary_tree.py
+++ Trees/binary_tree.py
@@ -1,3 +1,23 @@
+from abc import ABC, abstractmethod
+
+
+class BinaryTreeADT(ABC):
+    @abstractmethod
+    def insert(self, data):
+        """Insere elemento na árvore"""
+        pass
+
+    @abstractmethod
+    def empty(self):
+        """Verifica se a árvore está vazia"""
+        pass
+
+    @abstractmethod
+    def root(self):
+        """Retorna a raíz da árvore"""
+        pass
+
+
 class Node:
     def __init__(self, data=None, parent=None, left=None, right=None):
         self._data = data
@@ -9,27 +29,77 @@ class Node:
         print(self._data)
 
 
-class BinaryTree:
+class BinaryTree(BinaryTreeADT):
     def __init__(self, data=None):
         self._root = Node(data)
 
     def insert(self, data, _node=None):
-        _node = self._root
-        if _node is None:
-            _node = Node(data)
-        elif data < _node._data:
-            if _node._left:
-                self.insert(self,data,_node._left)
+        if self.empty():
+            self._root = Node(data)
+        else:
+            if not _node:
+                _node = self.root()
+            if _node._data >= data:
+                if not _node._left:
+                    _node._left = Node(data)
+                else:
+                    self.insert(data, _node._left)
             else:
-                _node._left = Node(data)
-        elif data > _node._data:
-            if _node._right:
-                self.insert(self, data, _node._right)
-            else:
-                _node._right = Node(data)
+                if not _node._right:
+                    _node._right = Node(data)
+                else:
+                    self.insert(data, _node._right)
+
+    def empty(self):
+        if self._root._data is None:
+            return True
+        return False
+
+    def root(self):
+        if not self.empty():
+            return self._root
+        return False
+
+    def inOrderTraverse(self, node=None, lista=list()):
+        if node is None:
+            node = self.root()
+        if node._left:
+            self.inOrderTraverse(node._left, lista)
+        lista.append(node._data)
+        if node._right:
+            self.inOrderTraverse(node._right, lista)
+        return lista
+
+    def preOrderTraverse(self, node=None, lista=list()):
+        if node is None:
+            node = self.root()
+        lista.append(node._data)
+        if node._left:
+            self.preOrderTraverse(node._left, lista)
+        if node._right:
+            self.preOrderTraverse(node._right, lista)
+        return lista
+
+    def postOrderTraverse(self, node=None, lista=list()):
+        if node is None:
+            node = self.root()
+        if node._left:
+            self.postOrderTraverse(node._left, lista)
+        if node._right:
+            self.postOrderTraverse(node._right, lista)
+        lista.append(node._data)
+        return lista
 
 
 if __name__ == '__main__':
     bt = BinaryTree(2)
     bt.insert(3)
-    print(bt)
+    bt.insert(7)
+    bt.insert(12)
+    bt.insert(5)
+    bt.insert(2)
+    bt.insert(6)
+    bt.insert(1)
+    print(bt.inOrderTraverse())
+    print(bt.preOrderTraverse())
+    print(bt.postOrderTraverse())
